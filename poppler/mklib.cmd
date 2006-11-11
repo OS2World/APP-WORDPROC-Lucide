648b41388d83a974cb49d11872a9923492789553
eros2 <eros2@2ebd8cc2-4212-0410-a26a-df1111805066>
2006-11-11 11:38:58 +0000

    cleanup make/.cmd

--- poppler/mklib.cmd
+++ poppler/mklib.cmd
@@ -1,4 +1,3 @@
 @echo off
 if exist poppler.lib del poppler.lib
-rem wlib poppler.lib + mypoppler\goo\libgoo.lib mypoppler\splash\libsplash.lib mypoppler\fofi\libfofi.lib mypoppler\poppler\libpoppler.lib fontconfig-2.3.2-os2\src\fontconfig.lib expat-1.95.8\lib\expat.lib freetype-2.1.10\src\ft2lib.lib
-wlib poppler.lib + mypoppler\goo\libgoo.lib mypoppler\splash\libsplash.lib mypoppler\fofi\libfofi.lib mypoppler\poppler\libpoppler.lib freetype-2.1.10\src\ft2lib.lib fc-emulate-os2\fontconfig\libfcos2.lib
+wlib poppler.lib + mypoppler\goo\libgoo.lib mypoppler\splash\libsplash.lib mypoppler\fofi\libfofi.lib mypoppler\poppler\libpoppler.lib freetype-2.2.1\src\ft2lib.lib fc-emulate-os2\fontconfig\libfcos2.lib
