702369c1acf89f68d5b5a42e605ee9b4626fd97c
eros2 <eros2@2ebd8cc2-4212-0410-a26a-df1111805066>
2006-11-18 13:00:38 +0000

    rename freetype dir

--- poppler/mklib.cmd
+++ poppler/mklib.cmd
@@ -1,3 +1,3 @@
 @echo off
 if exist poppler.lib del poppler.lib
-wlib poppler.lib + mypoppler\goo\libgoo.lib mypoppler\splash\libsplash.lib mypoppler\fofi\libfofi.lib mypoppler\poppler\libpoppler.lib freetype-2.2.1\src\ft2lib.lib fc-emulate-os2\fontconfig\libfcos2.lib
+wlib poppler.lib + mypoppler\goo\libgoo.lib mypoppler\splash\libsplash.lib mypoppler\fofi\libfofi.lib mypoppler\poppler\libpoppler.lib freetype2\src\ft2lib.lib fc-emulate-os2\fontconfig\libfcos2.lib
